9a9d17e0d70a63f574a1e433f0570455917a9f3e
Andres Felipe Orejuela <aorejuela@nexura.com>
2018-09-12 17:42:29 -0500

    Ajustes en la presentación del tablero.

--- tictactoe.py
+++ tictactoe.py
@@ -9,11 +9,15 @@ BOARD = [
     '| |', '| |', '| |',
     ]
 
+INDEX_COLUMN = ('0', '1', '2')
+INDEX_ROW = ('A', 'B', 'C')
+
 
 def display_board(board):
     """ Display the current view board."""
+    print(' |%s||%s||%s|' % INDEX_COLUMN)
     for row in range(0, 3):
-        display = ''
+        display = INDEX_ROW[row]
         for clm in range(0, 3):
             display += board[row*3 + clm]
         print(display)
